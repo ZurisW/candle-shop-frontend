348042d11d28b29139bac052689cb96941e30bc7
ZurisW <reksiu@onet.pl>
2025-09-11 02:53:44 +0200

    routes fix

--- angular-ecommerce/src/app/app.routes.server.ts
+++ angular-ecommerce/src/app/app.routes.server.ts
@@ -2,6 +2,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'products/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'categories/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'search/:keyword',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
